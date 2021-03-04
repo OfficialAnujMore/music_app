173bcfc5c65ed4b002d64a6a66ed97ab52e20e49
anujmore137 <moreanuj1307@gmail.com>
2021-03-04 20:01:40 +0530

    Update HomeScreen.dart

--- lib/screens/HomeScreen.dart
+++ lib/screens/HomeScreen.dart
@@ -57,7 +57,7 @@ class _HomeScreenState extends State<HomeScreen> {
             size: 30.0,
           ),
           title: Text(
-            "Trendings",
+            "Trending",
             style: MyTextStyle.text2,
           ),
         ),
